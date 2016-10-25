cbafbaf115817c962faf5a7f06f1840839567721
hoangtin1514 <1312600@student.hcmus.edu.vn>
2016-10-25 18:31:38 +0700

    Fix API GET Lich su

--- routes/Tinh.js
+++ routes/Tinh.js
@@ -4,15 +4,15 @@ var passport = require('passport');
 
 router.get('/api/v1/Tinh', function(req, res) {
     var T = req.db.get('Tinh');
-        T.find({"MaQG" : req.param('MaQG')}, function (err, result){    
+        T.find({"MaQG" : req.param('MaQG')}, function (err, result){
             if(err){
                 console.log(err);
-                res.send(500, 'something went wrong');               
+                res.send(500, 'something went wrong');
             } else {
-                console.log(result);        
+                console.log(result);
                 res.send(200, result);
             }
-            
+
         });
 });
 
